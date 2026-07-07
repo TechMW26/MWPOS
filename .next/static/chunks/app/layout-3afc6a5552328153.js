c0494ba0180dc0f4e8cf65f6295ce0f732601bc4
Aviraj <aviraj.sharma@mushroomworldgroup.com>
2026-07-07 15:30:43 +0530

    Update build manifests and reorder vendor chunks

---
+++ .next/static/chunks/app/layout-3afc6a5552328153.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[7177],{3673:()=>{},5891:(e,s,l)=>{Promise.resolve().then(l.t.bind(l,8963,23)),Promise.resolve().then(l.t.bind(l,3673,23))},8963:e=>{e.exports={style:{fontFamily:"'Inter', 'Inter Fallback'",fontStyle:"normal"},className:"__className_f367f3"}}},e=>{e.O(0,[7300,8441,1255,7358],()=>e(e.s=5891)),_N_E=e.O()}]);
